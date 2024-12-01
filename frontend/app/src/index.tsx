80e674cc6ac114be709d654db5494414001cf3e9
kutei <>
2024-12-01 15:12:38 +0900

    [change]Routerを追加

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Test from './Test';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
